36e53b11e89090dfe7fe49fd0c202e16d02ea11d
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-12-21 10:00:02 -0300

    Class User

--- src/main/java/com/avanade/dio/jwt/SpringBootJwtApplication.java
+++ src/main/java/com/avanade/dio/jwt/SpringBootJwtApplication.java
@@ -10,7 +10,7 @@ public class SpringBootJwtApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringBootJwtApplication.class, args);
-
+		UserData user = new UserData();
 
 
 
